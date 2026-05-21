8220f2380ebc7ca36bd604823c2aa16b1b6535ed
ariarainbow90-cmyk <ariarainbow90@gmail.com>
2026-05-21 09:11:54 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'tidal-v11';
+
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.url.includes('supabase.co')) return;
+});
